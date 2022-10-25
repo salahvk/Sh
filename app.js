1692bca87880cc9c05f1433810f37363c2613a2e
mo <salahkoyilandy@gmail.com>
2022-10-25 15:15:59 +0530

    mongo db added and data successfully entered

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 var hbs = require('express-handlebars')
 var userRouter = require('./routes/user');
 var adminRouter = require('./routes/admin');
+var fileUpload = require('express-fileupload')
+var db=require('./config/connection')
+
 
 var app = express();
 
@@ -18,7 +21,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(fileUpload())
+db.connect((err)=>{
+  if(err) console.log("Connection error");
+  else console.log("Database connected");
+})
 app.use('/', userRouter);
 app.use('/admin', adminRouter);
 
